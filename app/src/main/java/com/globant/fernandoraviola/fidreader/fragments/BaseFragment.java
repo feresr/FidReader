da46123d80ab802773725a5a86f090f696b523c1
Fernando Raviola <fernandoraviola@gmail.com>
2014-11-10 11:57:41 -0300

    Dismiss dialogs before upon exiting the app

--- app/src/main/java/com/globant/fernandoraviola/fidreader/fragments/BaseFragment.java
+++ app/src/main/java/com/globant/fernandoraviola/fidreader/fragments/BaseFragment.java
@@ -7,42 +7,55 @@ import android.support.v4.app.Fragment;
 
 /**
  * Created by fernando.raviola on 11/10/2014.
- *
+ * <p/>
  * This is intended as a base class for all fragments in the application and provides basic
  * functionality that should be present in all of them.
- *
  */
 public class BaseFragment extends Fragment {
 
-    protected ProgressDialog mDialog;
+    protected ProgressDialog mProgressDialog;
+    protected AlertDialog mErrorDialog;
 
     protected void showProgressDialog(int message) {
         if (getActivity() != null) {
-            mDialog = new ProgressDialog(getActivity());
-            mDialog.setIndeterminate(false);
-            mDialog.setCancelable(false);
-            mDialog.setMessage(getString(message));
-            mDialog.show();
+            mProgressDialog = new ProgressDialog(getActivity());
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setMessage(getString(message));
+            mProgressDialog.show();
         }
     }
 
     protected void dismissProgressDialog() {
-        if(mDialog.isShowing()) {
-            mDialog.dismiss();
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
         }
     }
 
     protected void showErrorDialog(int message) {
         // getActivity could return null if the device is rotated and the activity restored.
-        if(getActivity() != null) {
+        if (getActivity() != null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage(message)
                     .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                         }
                     });
-            builder.create().show();
+            mErrorDialog = builder.create();
+            mErrorDialog.show();
         }
     }
 
+    protected void dismissErrorDialog() {
+        if (mErrorDialog != null && mErrorDialog.isShowing()) {
+            mErrorDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        dismissProgressDialog();
+        dismissErrorDialog();
+        super.onPause();
+    }
 }
